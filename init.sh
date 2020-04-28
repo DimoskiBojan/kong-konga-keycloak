d2f4daac57c0f6186ef1d9b2678afb10d538e3d5
Bojan Dimoski <dimoski.b98@gmail.com>
2020-04-28 23:20:58 +0200

    fixed errors for initializing

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+docker-compose up -d kong-db
+docker-compose up -d keycloak-db
+sleep 1m
 cat dump_kong.sql | docker exec -i kong-konga-keycloak_kong-db_1 psql -U kong -d postgres
 cat dump_keycloak.sql | docker exec -i kong-konga-keycloak_keycloak-db_1 psql -U keycloak -d postgres
